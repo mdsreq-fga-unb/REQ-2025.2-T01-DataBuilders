e505de8d8ea1787ce77f35906fce7ca0d24d9008
Vinícius Ribeiro <viniciuspr2812@gmail.com>
2025-12-02 02:39:41 -0300

    Update package.json and server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,6 @@
 import app from './app';
 import { PORT } from './config';
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
+app.listen(Number(PORT), '0.0.0.0', () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
